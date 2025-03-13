176ac254dc09ef6a6e0d40e1150cff643cf89b9a
Bobcho101 <bgbobi878@gmail.com>
2025-03-13 17:11:38 +0200

    added register jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import Header from "./components/header/Header"
 import Home from './components/home/Home';
 import { Route, Routes } from 'react-router';
 import Create from './components/create-game/Create';
+import Games from './components/games/Games';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
 
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
             <Routes>
                 <Route path={'/'} element={<Home />} />
                 <Route path={'/create-game'} element={<Create />} />
+                <Route path={'/games'} element={<Games />} />
+                <Route path={'/register'} element={<Register />} />
+                <Route path={'/login'} element={<Login />} />
             </Routes>
         </>
     )
