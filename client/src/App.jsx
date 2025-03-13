c55bb5a9a8549809d43b6e5efe6fc31672569c82
Bobcho101 <bgbobi878@gmail.com>
2025-03-13 16:34:41 +0200

    init react router and added home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import './Typography.css';
 import './Global.css';
 import Header from "./components/header/Header"
 import Home from './components/home/Home';
+import { Route, Routes } from 'react-router';
+
 
 function App() {
     return (
         <>
-            <Header />  
-            <Home />
+        <Header /> 
+            <Routes>
+                <Route path={'/'} element={<Home />} />
+            </Routes>
         </>
     )
 }
