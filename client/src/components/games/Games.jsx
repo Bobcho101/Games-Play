d5a40eb887357c740d276648c3f9c5c11cfe3596
Bobcho101 <bgbobi878@gmail.com>
2025-03-14 14:33:08 +0200

    rendered games

--- client/src/components/games/Games.jsx
+++ client/src/components/games/Games.jsx
@@ -1,42 +1,53 @@
+import useFetch from "../../hooks/useFetch";
+
 export default function Games() {
-   return (
-       <>
-        <section id="catalog-page">
-            <h1>All Games</h1>
-            {/* Display div: with information about every game (if any) */}
-            <div className="allGames">
-                <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Cover Fire</h2>
-                <a href="#" className="details-button">
-                    Details
-                </a>
+    const [ data ] = useFetch('http://localhost:3030/jsonstore/games');
+
+    console.log(data);
+    
+    return (
+        <>
+            <section id="catalog-page">
+                <h1>All Games</h1>
+                {/* Display div: with information about every game (if any) */}
+                {data.length > 0 
+                ?    data.map((post) => (<div key={post[0]} className="allGames">
+                        <div className="allGames-info">
+                            <img src={post[1].imageUrl} />
+                            <h6>{post[1].category}</h6>
+                            <h2>{post[1].title}</h2>
+                            <a href="#" className="details-button">
+                                Details
+                            </a>
+                            </div>
+                        </div>
+                    ))
+                : <h3 className="no-articles">No articles yet</h3>
+                }
+               
+                {/* <div className="allGames">
+                    <div className="allGames-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <a href="#" className="details-button">
+                        Details
+                    </a>
+                    </div>
                 </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Zombie lang</h2>
-                <a href="#" className="details-button">
-                    Details
-                </a>
-                </div>
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>MineCraft</h2>
-                <a href="#" className="details-button">
-                    Details
-                </a>
-                </div>
-            </div>
-            {/* Display paragraph: If there is no games  */}
-            <h3 className="no-articles">No articles yet</h3>
-        </section>
-        </>
-   );
+                <div className="allGames">
+                    <div className="allGames-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>MineCraft</h2>
+                    <a href="#" className="details-button">
+                        Details
+                    </a>
+                    </div>
+                </div> */}
+                {/* Display paragraph: If there is no games  */}
+                {/* <h3 className="no-articles">No articles yet</h3> */}
+            </section>
+            </>
+    );
 }
