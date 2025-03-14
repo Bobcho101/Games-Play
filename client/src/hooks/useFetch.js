130897a9edcd91dd53a8b9ebd6b707401a820bac
Bobcho101 <bgbobi878@gmail.com>
2025-03-14 14:33:02 +0200

    fixed the useFetch

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -2,23 +2,20 @@ import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
     const [data, setData] = useState([]);
-    const [pending, setPending] = useState(false);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
-        setPending(true);
         fetch(url)
             .then((res) => res.json())
             .then((data) => {
-                setData(Object.values(data));
-                setPending(false);
+                console.log(Object.entries(data));
+                
+                setData(Object.entries(data));
             }).catch((err) => {     
-                setError(err.message);
-                setPending(false);
-            }).finally(() => setPending(false))
+                console.log(err.message);
+            })
     }, [url])
 
-    return [data, pending, error];
+    return [data];
 }
 
 export default useFetch;
