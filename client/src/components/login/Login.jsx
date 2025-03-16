da3abf437f780cc4fb5174be311f8c7df6149897
Bobcho101 <bgbobi878@gmail.com>
2025-03-16 12:19:44 +0200

    added formhandling

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,31 +1,43 @@
 import { Link } from "react-router";
+import useForm from "../../hooks/useForm";
 
 export default function Login() {
-   return (
-       <>
-           <section id="login-page" className="auth">
-            <form id="login">
-                <div className="container">
-                <div className="brand-logo" />
-                <h1>Login</h1>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Sokka@gmail.com"
-                />
-                <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" />
-                <input type="submit" className="btn submit" defaultValue="Login" />
-                <p className="field">
-                    <span>
-                    If you don&apos;t have profile click <Link to={'/register'}>here</Link>
-                    </span>
-                </p>
-                </div>
-            </form>
-        </section>
-       </>
-   );
+    const [formValues, changeFormValues] = useForm({
+        'email': '',
+        'password': '',
+    });
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        
+    }
+
+    return (
+        <>
+            <section id="login-page" className="auth">
+                <form onSubmit={formSubmitHandler} id="login">
+                    <div className="container">
+                    <div className="brand-logo" />
+                    <h1>Login</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Sokka@gmail.com"
+                        onChange={changeFormValues}
+                    />
+                    <label htmlFor="login-pass">Password:</label>
+                    <input type="password" id="login-password" name="password" onChange={changeFormValues} />
+                    <input type="submit" className="btn submit" defaultValue="Login" />
+                    <p className="field">
+                        <span>
+                        If you don&apos;t have profile click <Link to={'/register'}>here</Link>
+                        </span>
+                    </p>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
 }
