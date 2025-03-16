027ea33332442fa1c0f9327170fea27d082c41ac
Bobcho101 <bgbobi878@gmail.com>
2025-03-16 12:31:54 +0200

    added abort controller on the useFetch

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -2,15 +2,21 @@ import { useEffect, useState } from "react";
 
 const useFetch = (url, fetchingOneGame = false) => {
     const [data, setData] = useState(fetchingOneGame ? {} : []);
-
+    
    useEffect(() => {
-        fetch(url)
+        const controller = new AbortController();
+        const signal = controller.signal;
+        fetch(url, { signal })
             .then((res) => res.json())
             .then((data) => {
                 setData(fetchingOneGame ? data : Object.entries(data));
             }).catch((err) => {     
                 console.log(err.message);
             })
+
+        return () => {  
+            controller.abort(); 
+        };
     }, [url, fetchingOneGame]);
     
     return [data];
