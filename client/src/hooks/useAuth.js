9d8ed3197a74ac2062e921bba07ec5422e5a3769
Bobcho101 <bgbobi878@gmail.com>
2025-03-18 17:09:38 +0200

    fixed the useAuth hook

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,33 @@
+const baseUrl = "http://localhost:3030/users";
+
+const useLogin = () => {
+    const login = async (userValues) => {
+        try {
+            const res = await fetch(`${baseUrl}/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userValues)
+            });
+
+            if (!res.ok) {
+                throw new Error(`User does not exist!`);
+            }
+
+            const data = await res.json();
+            return data;
+        } catch (err) {
+            console.log(err.message);
+            return {error: err.message};
+        } 
+    }
+
+    return [login];
+}
+
+
+
+export {
+    useLogin,
+}
